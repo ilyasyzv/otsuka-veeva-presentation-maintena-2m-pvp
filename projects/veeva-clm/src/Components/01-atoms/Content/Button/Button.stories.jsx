c7da5e82d3b046620063b451013f9dd3939aefe0
Ievgen_Kyvgyla <Ievgen_Kyvgyla@epam.com>
2023-04-10 21:41:44 +0300

    DF-33017 | Added Headers and Buttons, fixed storybook

--- projects/veeva-clm/src/Components/01-atoms/Content/Button/Button.stories.jsx
+++ projects/veeva-clm/src/Components/01-atoms/Content/Button/Button.stories.jsx
@@ -9,3 +9,26 @@ export default {
 const Template = () => <Button text='Check In Now' />;
 
 export const Default = Template.bind({});
+
+export const Primary = {
+  args: {
+    custom: 'test1',
+    text: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    ...Primary.args,
+    text: '😄👍😍💯',
+    custom: 'test2',
+  },
+};
+
+export const Tertiary = {
+  args: {
+    ...Primary.args,
+    text: '📚📕📈🤓',
+    custom: 'test3',
+  },
+};
