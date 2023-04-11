369be0fe18eefe764184f73a9c033a9cd0007b30
Ievgen_Kyvgyla <Ievgen_Kyvgyla@epam.com>
2023-04-11 13:15:08 +0300

    DF-33019 | Added Modal component

---
+++ projects/veeva-clm/src/Components/04-templates/Layouts/Modal/Modal.stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Modal } from '@templates/Layouts/Modal/Modal';
+
+export default {
+  title: 'CLM/Templates/Modal',
+  component: Modal,
+};
+
+const Template = () => (<Modal />);
+
+export const Default = Template.bind({});
+
+
