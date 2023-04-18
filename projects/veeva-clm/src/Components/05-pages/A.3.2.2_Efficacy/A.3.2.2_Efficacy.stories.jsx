77948bb4e515ca2b21686dcd155c55d55f0b5805
Ilias Iangurazov <ilias.langurazov@pearson.com>
2023-04-18 07:27:01 +0600

    DF-33507 | Add Footer to A.3.2.2_Efficacy

--- projects/veeva-clm/src/Components/05-pages/A.3.2.2_Efficacy/A.3.2.2_Efficacy.stories.jsx
+++ projects/veeva-clm/src/Components/05-pages/A.3.2.2_Efficacy/A.3.2.2_Efficacy.stories.jsx
@@ -6,6 +6,7 @@ import TableImg from '@/Components/05-pages/A.3.2.2_Efficacy/img/Table.png?forma
 import TableBannerImg from '@/Components/05-pages/A.3.2.2_Efficacy/img/TableBanner.png?format=webp';
 
 import './A.3.2.1_Efficacy.scss';
+import { Footer } from '@/Components/03-organisms/Footer/Footer';
 
 export default {
   title: 'CLM/Pages',
@@ -29,6 +30,77 @@ const content = () => (
     </Header>
     <img src={TableImg} alt='ratio table' className='mb-50' />
     <img src={TableBannerImg} alt='banner' className='mb-20' />
+    <Footer
+      footnotes={{
+        data: [
+          {
+            supLeft: '*',
+            innerText: (
+              <span>
+                AUC<sup>0-56</sup> following the fourth administration of
+                ABILIFY ASIMTUFII 960 mg or the sum of AUC<sup>0-28</sup>{' '}
+                following the seventh and eighth administrations of ABILIFY
+                MAINTENA 400 mg.
+              </span>
+            ),
+          },
+          {
+            supLeft: '†',
+            innerText: (
+              <span>
+                Aripiprazole plasma concentrations following the fourth
+                administration of ABILIFY ASIMTUFII 960 mg (C<sup>56</sup>) or
+                the eighth administration of ABILIFY MAINTENA 400 mg (C
+                <sup>28</sup>).
+              </span>
+            ),
+          },
+          {
+            supLeft: '‡',
+            innerText: (
+              <span>
+                ABILIFY ASIMTUFII 960 mg (n=34), ABILIFY MAINTENA 400 mg (n=32).
+              </span>
+            ),
+          },
+          {
+            supLeft: '§',
+            innerText: (
+              <span>
+                ABILIFY ASIMTUFII 960 mg (n=96), ABILIFY MAINTENA 400 mg (n=82).
+              </span>
+            ),
+          },
+          {
+            innerText: (
+              <span>
+                AUC=area under curve; C=concentration; CI=confidence interval; C
+                <sup>max</sup>=maximum concentration.
+              </span>
+            ),
+          },
+        ],
+      }}
+      safetyInfo={{
+        custom: 'mt-25',
+        data: [
+          {
+            text: (
+              <span>
+                Please see{' '}
+                <a href='/' className='blue-500'>
+                  IMPORTANT SAFETY INFORMATION
+                </a>{' '}
+                in this presentation.
+              </span>
+            ),
+          },
+          {
+            text: 'FOR USE WITH FORMULARY DECISION-MAKERS ONLY. DO NOT DUPLICATE OR DISTRIBUTE. ',
+          },
+        ],
+      }}
+    />
   </div>
 );
 
