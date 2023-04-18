9a997ca9619c1bd3a3f6efb226bffbf9c8272119
Nikolai Tatianenko <nikolai_tatianenko@epam.com>
2023-04-18 03:24:54 +0200

    DF-33386 | A.3.0_Clinical_profile

--- projects/veeva-clm/src/Components/05-pages/A.3.0_Clinical_profile/A.3.0_Clinical_profile.jsx
+++ projects/veeva-clm/src/Components/05-pages/A.3.0_Clinical_profile/A.3.0_Clinical_profile.jsx
@@ -1,36 +1,45 @@
 import Header from "@/Components/01-atoms/Content/Header/Header";
 import React from "react";
 import styles from "./A.3.0_Clinical_profile.module.scss";
+import Button from "@/Components/01-atoms/Content/Button/button";
 
 const PageContent = () => {
   return (
     <div className={styles.page}>
-      <Header type="h1">
-        ABILIFY ASIMTUFII® (aripiprazole): The<br /> First and Only
-        Once-Every-2-Months Long-<br />Acting Injectable Suspension Indicated
-        for<br />Treatment of Both Schizophrenia and<br />Bipolar I Disorder in
-        Adults
-      </Header>
-      <div className={"buttons-group"}>
-        <div className={"buttons-group-row"}>
-          <Link to={"A.3.0_Clinical_profile"}
-                custom={"button__type-c button__type-c-large button__type-c-large-blue"}>Overview</Link>
-          <Link to={"A.3.1_Clinical_overview"}
-                custom={"button__type-c button__type-c-large button__type-c-large-blue"}>PK
-            Study</Link>
-          <Link to={"A.3.2.1_Efficacy"}
-                custom={"button__type-c button__type-c-large button__type-c-large-blue"}>ABILIFY
-            MAINTENA® Pivotal Studies</Link>
+        <Header type="h1">
+          ABILIFY ASIMTUFII<sup>®</sup> (aripiprazole): The<br /> First and Only
+          Once-Every-2-Months Long-<br />Acting Injectable Suspension Indicated
+          for<br />Treatment of Both Schizophrenia and<br />Bipolar I Disorder
+          in
+          Adults
+        </Header>
+        <div className={"buttons-group"}>
+          <div className={"buttons-group-row"}>
+            <Button
+              custom={"button__type-c button__type-c-large button__type-c-large-blue"}
+              text={"Overview"} />
+            <Button to={"A.3.1_Clinical_overview"}
+                    custom={"button__type-c button__type-c-large button__type-c-large-blue"}
+                    text={"PK Study"}
+            />
+            <Button to={"A.3.2.1_Efficacy"}
+                    custom={"button__type-c button__type-c-large button__type-c-large-blue"}
+                    text={"ABILIFY MAINTENA® Pivotal Studies"}
+            />
+          </div>
+          <div className={"buttons-group-row"}>
+            <Button to={"A.3.4_Dosing"}
+                    custom={"button__type-c button__type-c-large button__type-c-large-blue"}
+                    text={"Dosing & Administration"}
+            />
+            <Button to={"A.3.5.1_Additional"}
+                    custom={"button__type-c button__type-c-large button__type-c-large-blue"}
+                    text={"ABILIFY MAINTENA RWE"}
+            />
+          </div>
         </div>
-        <div className={"buttons-group-row"}>
-          <Link to={"A.3.4_Dosing"}
-                custom={"button__type-c button__type-c-large button__type-c-large-blue"}>Dosing
-            & Administration</Link>
-          <Link to={"A.3.5.1_Additional"}
-                custom={"button__type-c button__type-c-large button__type-c-large-blue"}>ABILIFY
-            MAINTENA RWE</Link>
-        </div>
-      </div>
-    </div>);
+
+    </div>
+  );
 };
 export default PageContent;
