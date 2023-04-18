28d9562cac90bf6c12eee6a8bb870f4a4973438a
Ievgen_Kyvgyla <Ievgen_Kyvgyla@epam.com>
2023-04-18 10:33:37 +0300

    DF-33512 | DF-33513 | Added footer

--- projects/veeva-clm/src/Components/05-pages/A.3.5.3_Additional/A.3.5.3_Additional.stories.jsx
+++ projects/veeva-clm/src/Components/05-pages/A.3.5.3_Additional/A.3.5.3_Additional.stories.jsx
@@ -8,6 +8,8 @@ import {
 } from '@templates/Layouts/Modal/Modal.stories';
 import { DefaultOne } from '@organisms/CollapsibleGroup/CollapsibleGroup.stories';
 import './style.scss';
+import { Footer } from '@/Components/03-organisms/Footer/Footer';
+import MaintenaLogo from '@/assets/maintena-logo.png';
 
 export default {
   title: 'CLM/Pages',
@@ -32,6 +34,61 @@ const content = () => (
     <StudyDesignSup {...StudyDesignSup.args} />
     <DefaultOne {...DefaultOne.args} />
     <UnderstandCGIS {...UnderstandCGIS.args} />
+    <Footer
+      footnotes={{
+        data: [
+          {
+            supLeft: '*',
+            innerText:
+              'Baseline characteristics: PANSS total score ≥80 and a PANSS score >4 on each of 4 specific psychotic symptoms (conceptual disorganization, hallucinatory behavior, suspiciousness/persecution, and unusual thought content) at screening and baseline; diagnosis of schizophrenia ≥1 year.',
+          },
+          {
+            supLeft: '†',
+            innerText: 'P<0.001.',
+          },
+          {
+            supLeft: '‡',
+            innerText: 'P<0.0001.',
+          },
+          {
+            innerText:
+              'CGI-S=Clinical Global Impression-Severity of Illness Scale; PANSS=Positive and Negative Syndrome Scale; SD=standard deviation.',
+          },
+        ],
+      }}
+      references={{
+        data: [
+          {
+            text: (
+              <span>
+                Kane JM, et al. <em>J Clin Psychiatry.</em>{' '}
+                2014;75(11):1254-1260.
+              </span>
+            ),
+          },
+        ],
+      }}
+      safetyInfo={{
+        custom: 'mt-5',
+        data: [
+          {
+            text: (
+              <span>
+                Please see{' '}
+                <a href='/' className='blue-500'>
+                  IMPORTANT SAFETY INFORMATION
+                </a>{' '}
+                in this presentation.
+              </span>
+            ),
+          },
+          {
+            text: 'FOR USE WITH FORMULARY DECISION-MAKERS ONLY. DO NOT DUPLICATE OR DISTRIBUTE. ',
+          },
+        ],
+      }}
+      image={<img src={MaintenaLogo} alt='maintena-logo' width='140.2' />}
+    />
   </>
 );
 
