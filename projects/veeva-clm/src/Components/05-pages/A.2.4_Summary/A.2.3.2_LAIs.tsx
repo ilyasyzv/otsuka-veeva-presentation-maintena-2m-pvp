a19551eaa36b872f45b518dd221c93e477ab1994
Katsiaryna_Matsveyeva <katsiaryna_matsveyeva@epam.com>
2023-04-19 15:17:16 +0600

    DF-33757 | DF-33756 | update summary and clinical profile

--- projects/veeva-clm/src/Components/05-pages/A.2.4_Summary/A.2.3.2_LAIs.tsx
+++ projects/veeva-clm/src/Components/05-pages/A.2.4_Summary/A.2.3.2_LAIs.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import Header from '@/Components/01-atoms/Content/Header/Header';
-import imageTable from './imgs/A.2.3.2_LAIs.png';
-import styles from './A.2.3.2_LAIs.module.scss';
 import { Footer } from '@/Components/03-organisms/Footer/Footer';
 
-const PageContent = () => (<div className={styles.page}>
-    <Header type="h1">
-      There remains an unmet need for schizophrenia and bipolar I<br/>disorder
-      (BP-I)* to improve treatment adherence and reduce<br/>health care
-      resource
-      utilization and costs
+import imageTable from './imgs/A.2.3.2_LAIs.png';
+
+import styles from './A.2.3.2_LAIs.module.scss';
+
+const PageContent = () => (
+  <div className={styles.page}>
+    <Header type='h1'>
+      <>
+        There remains an unmet need for schizophrenia and bipolar I<br />
+        disorder (BP-I)* to improve treatment adherence and reduce
+        <br />
+        health care resource utilization and costs<sup>1-14</sup>
+      </>
     </Header>
-    <img src={imageTable} alt={'Table'} className={'pageImg'}/>
+    <img src={imageTable} alt='Table' className='pageImg' />
     <Footer
-      custom="bottom-5"
+      custom='bottom-5'
       footnotes={{
         data: [
           {
             supLeft: '*',
-            innerText: (<><span>These studies do not differentiate between bipolar disorders.</span><br/></>),
-          }, {
-            innerText: (<span>FDA=US Food and Drug Administration; LAI=long-acting injectable.</span>),
+            innerText: (
+              <>
+                <span>
+                  These studies do not differentiate between bipolar disorders.
+                </span>
+                <br />
+              </>
+            ),
+          },
+          {
+            innerText: (
+              <span>
+                FDA=US Food and Drug Administration; LAI=long-acting injectable.
+              </span>
+            ),
           },
         ],
       }}
@@ -29,7 +46,9 @@ const PageContent = () => (<div className={styles.page}>
           {
             text: (
               <span>
-                Treatment Advocacy Center. https://www.treatmentadvocacycenter.org/evidence-and-research/fast-facts. Accessed February 15, 2023.
+                Treatment Advocacy Center.
+                https://www.treatmentadvocacycenter.org/evidence-and-research/fast-facts.
+                Accessed February 15, 2023.
               </span>
             ),
           },
@@ -64,20 +83,25 @@ const PageContent = () => (<div className={styles.page}>
           {
             text: (
               <span>
-                Goldstone LW. <em>Am J Manag Care</em>. 2020;26(3 suppl):S48-S54.
+                Goldstone LW. <em>Am J Manag Care</em>. 2020;26(3
+                suppl):S48-S54.
               </span>
             ),
           },
           {
             text: (
               <span>
-                Kadakia A, et al. <em>J Clin Psychiatry</em>. 2022;83(6):22m14458.
+                Kadakia A, et al. <em>J Clin Psychiatry</em>.
+                2022;83(6):22m14458.
               </span>
             ),
           },
           {
             text: (
-              <span>Bessonova L, et al. <em>Clinicoecon Outcomes Res</em>. 2020;12:481-497.</span>
+              <span>
+                Bessonova L, et al. <em>Clinicoecon Outcomes Res</em>.
+                2020;12:481-497.
+              </span>
             ),
           },
           {
@@ -89,27 +113,49 @@ const PageContent = () => (<div className={styles.page}>
           },
           {
             text: (
-              <span>Zolezzi M, et al. <em>Neuropsychiatr Dis Treat</em>. 2021;17:1917-1926.</span>
+              <span>
+                Zolezzi M, et al. <em>Neuropsychiatr Dis Treat</em>.
+                2021;17:1917-1926.
+              </span>
             ),
           },
           {
             text: (
-              <span>Greene M, et al. <em>J<br/> Med Econ</em>. 2018;21(2):127-134.</span>
+              <span>
+                Greene M, et al.{' '}
+                <em>
+                  J<br /> Med Econ
+                </em>
+                . 2018;21(2):127-134.
+              </span>
             ),
           },
           {
             text: (
-              <span>Lafeuille M-H, et al. <em>BMC Psychiatry</em>. 2013;13:221.</span>
+              <span>
+                Lafeuille M-H, et al. <em>BMC Psychiatry</em>. 2013;13:221.
+              </span>
             ),
           },
-          
+
           {
             text: (
-              <span>Pietrini F, et al. <em>Neuropsychiatr Dis Treat</em>. 2019;15:1045-1060.</span>
+              <span>
+                Pietrini F, et al. <em>Neuropsychiatr Dis Treat</em>.
+                2019;15:1045-1060.
+              </span>
             ),
-          }, {
+          },
+          {
             text: (
-              <span>American Psychiatric Association. <em>Practice Guidelines for the Treatment of Patients With Schizophrenia</em>. 3rd ed. Washington, DC; 2021.</span>
+              <span>
+                American Psychiatric Association.{' '}
+                <em>
+                  Practice Guidelines for the Treatment of Patients With
+                  Schizophrenia
+                </em>
+                . 3rd ed. Washington, DC; 2021.
+              </span>
             ),
           },
         ],
