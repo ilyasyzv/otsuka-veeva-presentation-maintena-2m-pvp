6463c9a638565f74e54c0f85efc6d7a4e51837d9
Ievgen_Kyvgyla <Ievgen_Kyvgyla@epam.com>
2023-04-25 13:46:50 +0300

    DF-34030 | Removed extra coma in ISI page

--- projects/veeva-clm/src/Components/05-pages/A.7.0_ISI/A.7.0_ISI.stories.jsx
+++ projects/veeva-clm/src/Components/05-pages/A.7.0_ISI/A.7.0_ISI.stories.jsx
@@ -274,7 +274,7 @@ const content = () => (
                 <Link to='A.7.0_ISI' custom='blue-500'>
                   IMPORTANT SAFETY INFORMATION
                 </Link>{' '}
-                , in this presentation.
+                in this presentation.
               </span>
             ),
           },
